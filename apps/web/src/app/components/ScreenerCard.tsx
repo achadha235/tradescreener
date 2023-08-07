5ac8e642f40b402fa3da4ab0c35bc0eba2a574b7
Abhishek Chadha <abhishekchadha235@gmail.com>
2023-08-07 12:57:24 -0700

    fix line overflow

--- apps/web/src/app/components/ScreenerCard.tsx
+++ apps/web/src/app/components/ScreenerCard.tsx
@@ -7,7 +7,7 @@ export function ScreenerCard({ screener, target = "_self" }) {
       <Link target={target} key={screener.id} href={`/screener/${screener.id}`}>
         <div className="w-full h-full p-4 bg-neutral-800 flex flex-col gap-4 row-span-1 justify-start items-start shadow-lg border-[2px] border-solid border-neutral-600 hover:border-neutral-300 transition-colors rounded-lg">
           <div className="text-2xl">{screener.screenerData.name}</div>
-          <div className="text-lg font-normal">
+          <div className="text-lg font-normal line-clamp-6 overflow-ellipsis">
             {screener.screenerData.userRequest.screenerPrompt}
           </div>
 
